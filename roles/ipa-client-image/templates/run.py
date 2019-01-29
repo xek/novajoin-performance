057a6b9c1378760133cfdc72fab4350e503cee29
Grzegorz Grasza <xek@redhat.com>
2019-01-29 19:52:00 +0100

    Authenticate in Keystone, tested

--- roles/ipa-client-image/templates/run.py
+++ roles/ipa-client-image/templates/run.py
@@ -7,6 +7,49 @@ from time import sleep
 from urllib.request import Request, urlopen
 
 
+### Set DNS
+
+open('/etc/resolv.conf', 'w+').write('''
+search example.test
+nameserver {{ ipa_dns }}
+''')
+
+### Authenticate in Keystone
+
+credentials = {
+    "auth": {
+        "identity": {
+            "methods": [
+                "password"
+            ],
+            "password": {
+                "user": {
+                    "name": "nova",
+                    "domain": {
+                        "name": "Default"
+                    },
+                    "password": "{{ nova_password }}"
+                }
+            }
+        },
+        "scope": {
+            "project": {
+                "name": "service",
+                "domain": {
+                    "id": "default"
+                }
+            }
+        }
+    }
+}
+
+headers = {'Content-Type': 'application/json'}
+request = Request('{{ keystone_url }}/v3/auth/tokens',
+                  json.dumps(credentials).encode(), headers)
+headers['X-Auth-Token'] = urlopen(request).info().get('X-Subject-Token')
+
+### Create metadata with services, get hostname
+
 example_services = ['mysql', 'rabbitmq', 'redis', 'neutron', 'novnc-proxy',
                     'HTTP', 'libvirt-vnc', 'haproxy']
 
@@ -17,11 +60,19 @@ for service in example_services:
 
 params = {"metadata": metadata, "hostname": hostname}
 
-request = Request('{{ novajoin_url }}', json.dumps(params).encode())
+### Get ipaotp from novajoin
+
+request = Request('{{ novajoin_url }}/v1/', json.dumps(params).encode(), headers)
 result = json.loads(urlopen(request).read().decode())
 
-system('ipa-client-install -U -w {ipaotp} '
-       '--hostname {hostname} --realm {krb_realm}'.format(result))
+result['domain'] = result['krb_realm'].lower()
+
+### Run IPA Client installation
+
+system('ipa-client-install -U -N -w {ipaotp} --domain {domain} '
+       '--hostname {hostname} --realm {krb_realm}'.format(**result))
+
+### Request certificates
 
 for service in example_services:
     system('ipa-getcert request -r '
@@ -29,10 +80,10 @@ for service in example_services:
            '-k /etc/pki/tls/private/{hostname}-{service}.key '
            '-N CN={hostname}.test.{domain} -D {hostname}.{domain} '
            '-U id-kp-serverAuth '
-           '-K {service}/{hostname}.test.{domain}`'.format({
-               'hostname': hostname,
-               'domain': result['krb_realm'].lower(),
-               'service': service}))
+           '-K {service}/{hostname}.test.{domain}'.format(
+               hostname=hostname,
+               domain=result['domain'],
+               service=service))
 
 sleep(60)
 system('ipa-getcert list')
